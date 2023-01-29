63e37d351a1aacc281e7aa701cd5c5c561758cac
fkordell <far18009@byui.edu>
2023-01-28 19:35:11 -0700

    start to my home page

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,35 @@
+// date //
+const datefield = document.querySelector(".date");
+const datefieldUK = document.querySelector("aside");
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+// last updated //
+let lastModified = new Date(document.lastModified);
+let fullDate = lastModified.toLocaleString('en-US', {month: "2-digit", day: "2-digit", year: "numeric"});
+let time = lastModified.toLocaleString('en-GB', {hour: "2-digit", minute: "2-digit", second: "2-digit"});
+let dateTime = `Last Updated: ${fullDate} ${time}`;
+document.getElementById("lastModified").innerHTML = dateTime;
+
+
+
+// nav //
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn")
+x.onclick = toggleMenu;
+
+
+
+// copyright//
+document.getElementById("year").innerHTML = new Date().getFullYear();

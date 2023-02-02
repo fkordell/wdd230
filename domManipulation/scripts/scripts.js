677e71e40d6bbd38f3360c0aa82099805c166fa1
fkordell <far18009@byui.edu>
2023-02-02 16:45:18 -0700

    bom manipulation commit

---
+++ domManipulation/scripts/scripts.js
@@ -0,0 +1,20 @@
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const list = document.querySelector("ul");
+
+button.addEventListener("click", () => {
+  if (input.value) {
+    const listItem = document.createElement("li");
+    const listText = document.createElement("span");
+    const deleteButton = document.createElement("button");
+
+    listText.innerHTML = input.value;
+    listItem.append(listText, deleteButton);
+    deleteButton.textContent = "❌";
+    list.appendChild(listItem);
+
+    deleteButton.addEventListener("click", () => list.removeChild(listItem));
+    input.focus();
+    input.value = "";
+  }
+});
